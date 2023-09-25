c4df25d65411faaf7dec3cb8dc95e537dac967b7
adsyam <yambaoadrianne@gmail.com>
2023-09-26 00:31:50 +0800

    Fixed all formats

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { services } from "../constants";
-import ServiceCard from "../components/ServiceCard";
+import React from 'react'
+import ServiceCard from '../components/ServiceCard'
+import { services } from '../constants'
 
 const Services = () => {
-	return (
-		<section className="max-container flex justify-center flex-wrap gap-9">
-			{services.map((service) => (
-				<ServiceCard key={service.label} {...service} />
-			))}
-		</section>
-	);
-};
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map( service => (
+        <ServiceCard key={service.label} {...service} />
+      ) )}
+    </section>
+  )
+}
 
-export default Services;
+export default Services
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontSize: {
@@ -22,21 +22,21 @@ export default {
         montserrat: ['Montserrat', 'sans-serif'],
       },
       colors: {
-        'primary': "#ECEEFF",
-        "coral-red": "#FF6452",
-        "slate-gray": "#6D6D6D",
-        "pale-blue": "#F5F6FF",
-        "white-400": "rgba(255, 255, 255, 0.80)"
+        primary: '#ECEEFF',
+        'coral-red': '#FF6452',
+        'slate-gray': '#6D6D6D',
+        'pale-blue': '#F5F6FF',
+        'white-400': 'rgba(255, 255, 255, 0.80)'
       },
       boxShadow: {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
       },
       backgroundImage: {
-        'hero': "url('assets/images/collection-background.svg')",
-        'card': "url('assets/images/thumbnail-background.svg')",
+        hero: 'url(\'assets/images/collection-background.svg\')',
+        card: 'url(\'assets/images/thumbnail-background.svg\')',
       },
       screens: {
-        "wide": "1440px"
+        wide: '1440px'
       }
     },
   },
